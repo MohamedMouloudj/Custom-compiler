9b8bf2b0b4a47a6575996ffac67b86b055abd526
Hichem-Brahim_Djelloul <162839696+Hichem-BRHM@users.noreply.github.com>
2024-11-29 16:17:27 +0100

    Update SymbolTable.java

--- src/main/java/com/customCompiler/SymbolTable.java
+++ src/main/java/com/customCompiler/SymbolTable.java
@@ -12,3 +12,46 @@ public class SymbolTable {
         return Expression.ExpressionType.INTEGER.toString();
     }
 }
+
+
+
+
+
+/* package com.customCompiler.gen;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SymbolTable {
+    private Map<String, Symbol> symbols;
+
+    public SymbolTable() {
+        symbols = new HashMap<>();
+    }
+
+    public void addSymbol(String name, Symbol symbol) {
+        if (symbols.containsKey(name)) {
+            throw new RuntimeException("Symbol " + name + " is already declared.");
+        }
+        symbols.put(name, symbol);
+    }
+
+    public Symbol getSymbol(String name) {
+        return symbols.get(name);
+    }
+
+    public boolean containsSymbol(String name) {
+        return symbols.containsKey(name);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Symbol Table:\n");
+        for (Symbol symbol : symbols.values()) {
+            sb.append(symbol.toString()).append("\n");
+        }
+        return sb.toString();
+    }
+}
+*/
